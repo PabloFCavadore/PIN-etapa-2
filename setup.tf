3c556d377cd84291b471ba27fac0aa4c8b4d7629
PabloFCavadore <156964534+PabloFCavadore@users.noreply.github.com>
2024-08-06 20:53:06 -0300

    Update setup.tf

--- setup.tf
+++ setup.tf
@@ -10,20 +10,25 @@ provider "aws" {
 #}
 
 #Create VPC in us-east-1
-resource "aws_vpc" "vpc" {
-  cidr_block           = "10.0.0.0/16"
-  enable_dns_support   = true
-  enable_dns_hostnames = true
-  tags = {
-    Name = "terraform-vpc"
-  }
+#resource "aws_vpc" "vpc" {
+#  cidr_block           = "10.0.0.0/16"
+#  enable_dns_support   = true
+ # enable_dns_hostnames = true
+  #tags = {
+ #   Name = "terraform-vpc"
+#  }
+#
+#}
 
-}
+#usamos la vpc proporcionada
+data "aws_vpc" "existing_vpc" { id = "vpc-0583a731fd747f71f" }
 
 #Create IGW in us-east-1
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.vpc.id
 }
+
+
 
 #Get main route table to modify
 data "aws_route_table" "main_route_table" {
@@ -54,11 +59,11 @@ data "aws_availability_zones" "azs" {
 }
 
 #Create subnet # 1 in us-east-1
-resource "aws_subnet" "subnet" {
-  availability_zone = element(data.aws_availability_zones.azs.names, 0)
-  vpc_id            = aws_vpc.vpc.id
-  cidr_block        = "10.0.1.0/24"
-}
+#resource "aws_subnet" "subnet" {
+ # availability_zone = element(data.aws_availability_zones.azs.names, 0)
+  #vpc_id            = aws_vpc.vpc.id
+ # cidr_block        = "10.0.1.0/24"
+#}
 
 
 #Create SG for allowing TCP/80 & TCP/22
