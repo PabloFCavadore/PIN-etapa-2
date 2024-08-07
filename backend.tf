efa0e2e3f3de5b054d6e186e784094f638246b4c
PabloFCavadore <156964534+PabloFCavadore@users.noreply.github.com>
2024-08-06 21:26:05 -0300

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3"{
-    bucket                 = "bucket1"
+    bucket                 = "mundose-tf-2024"
     region                 = "us-east-1"
     key                    = "backend.tfstate"
-    dynamodb_table         = "terraformstatelock"
+    #dynamodb_table         = "terraformstatelock"
   }
 }
 
